efa6d666e9e1167c0294c4445db96946ba32cefd
Yariangel Aray <gabrielamunar4@gmail.com>
2025-07-07 12:14:47 -0500

    Perfil del usuario

--- Vista/JS/estilos.js
+++ Vista/JS/estilos.js
@@ -1,16 +1,16 @@
-const contrasena = document.querySelector("#contrasena");
-const iconoPass = document.querySelector("input[name='contrasena'] + .icon");
+const contrasena = document.querySelector('#contrasena');
+const iconoPass = document.querySelector('input[name="contrasena"] + .icon');
 
-iconoPass.addEventListener("click", () => {
-  if (contrasena.type == "password") {
-    contrasena.type = "text";
-    iconoPass.classList.remove("ri-eye-line");
-    iconoPass.classList.add("ri-eye-close-line");
+iconoPass.addEventListener('click', () => {
+  if (contrasena.type == 'password') {
+    contrasena.type = 'text';
+    iconoPass.classList.remove('ri-eye-line');
+    iconoPass.classList.add('ri-eye-close-line');
   }
   else {
-    contrasena.type = "password"
-    iconoPass.classList.remove("ri-eye-close-line");
-    iconoPass.classList.add("ri-eye-line");
+    contrasena.type = 'password'
+    iconoPass.classList.remove('ri-eye-close-line');
+    iconoPass.classList.add('ri-eye-line');
   }
 })
 
